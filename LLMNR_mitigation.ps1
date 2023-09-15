efa82fb4f3af113101d05de78d25e17cadc12d40
ThinKing19 <79785240+ThinKing19@users.noreply.github.com>
2023-09-15 23:25:28 +0200

    Update LLMNR_mitigation.ps1
    comments in english

--- LLMNR_mitigation.ps1
+++ LLMNR_mitigation.ps1
@@ -14,15 +14,15 @@ New-NetFirewallRule -Name "Block LLMNR" -DisplayName "Block LLMNR" -Direction In
 New-NetFirewallRule -Name "Block LLMNR" -DisplayName "Block LLMNR" -Direction Outbound -Protocol UDP -LocalPort 5355 -Action Block
 
 
-# Questo script eseguirà le seguenti azioni:
-# - Disabiliterà LLMNR su tutti gli adattatori di rete che supportano il protocollo.
-# - Imposterà i server DNS su 1.1.1.1 e 8.8.8.8.
-# - Imposterà i timeout di acquisizione, rinnovo e rilascio del lease DHCP su 1 minuto.
-# - Creerà due regole di firewall per bloccare il traffico LLMNR in ingresso e in uscita.
-# È importante notare che questo script è solo una guida. Potrebbe essere necessario modificarlo per soddisfare le specifiche esigenze della tua rete.
+# This script will perform the following actions:
+# - Will disable LLMNR on all network adapters that support the protocol.
+# - Will set DNS servers to 1.1.1.1 and 8.8.8.8.
+# - Will set the DHCP lease acquisition, renewal, and release timeouts to 1 minute.
+# - Will create two firewall rules to block incoming and outgoing LLMNR traffic.
+# It is important to note that this script is only a guide. You may need to modify this to meet the specific needs of your network.
 
 
-# La prima parte dello script utilizza il cmdlet Get-NetAdapter per ottenere una lista di tutti gli adattatori di rete. Quindi, utilizza il cmdlet Where-Object per filtrare la lista in base alla proprietà LinkLayerTopologyDescription. Questa proprietà indica se l'adattatore supporta il protocollo LLMNR. Se l'adattatore supporta LLMNR, il cmdlet Set-NetAdapter lo disabiliterà.
-# La seconda parte dello script utilizza il cmdlet Set-DnsClientServerAddress per impostare i server DNS. In questo caso, i server DNS sono impostati su 1.1.1.1 e 8.8.8.8.
-# La terza parte dello script utilizza i cmdlet Set-DnsClientLeaseObtainTimeout, Set-DnsClientLeaseRenewTimeout e Set-DnsClientLeaseReleaseTimeout per impostare i timeout di acquisizione, rinnovo e rilascio del lease DHCP. Questi timeout sono impostati su 1 minuto per rendere più difficile per gli aggressori utilizzare indirizzi IP autoassegnati per gli attacchi LLMNR Poisoning.
-# L'ultima parte dello script utilizza il cmdlet New-NetFirewallRule per creare due regole di firewall che bloccano il traffico LLMNR in ingresso e in uscita.
+# The first part of the script uses the Get-NetAdapter cmdlet to get a list of all network adapters. Then, use the Where-Object cmdlet to filter the list based on the LinkLayerTopologyDescription property. This property indicates whether the adapter supports the LLMNR protocol. If the adapter supports LLMNR, the Set-NetAdapter cmdlet will disable it.
+# The second part of the script uses the Set-DnsClientServerAddress cmdlet to set DNS servers. In this case, the DNS servers are set to 1.1.1.1 and 8.8.8.8.
+# The third part of the script uses the Set-DnsClientLeaseObtainTimeout, Set-DnsClientLeaseRenewTimeout, and Set-DnsClientLeaseReleaseTimeout cmdlets to set the DHCP lease acquisition, renewal, and release timeouts. These timeouts are set to 1 minute to make it more difficult for attackers to use self-assigned IP addresses for LLMNR Poisoning attacks.
+# The last part of the script uses the New-NetFirewallRule cmdlet to create two firewall rules that block incoming and outgoing LLMNR traffic.
